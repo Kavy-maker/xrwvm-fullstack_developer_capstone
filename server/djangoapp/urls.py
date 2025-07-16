8514094d0d0ff4e1ca0dbbd291c9f420350d3aa6
Kavy-maker <kavitayvonne@gmail.com>
2025-07-16 05:00:09 -0400

    Initial commit

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -9,6 +9,7 @@ from . import views
 app_name = 'djangoapp'
 urlpatterns = [
     # # path for registration
+    
 
     # path for login
     # API endpoint for login authentication (called by React frontend)
@@ -17,6 +18,10 @@ urlpatterns = [
 
     # Path for serving the React login page
     path('login/', TemplateView.as_view(template_name="index.html"), name='react_login'),
+    
+    #path for logout
+    path('logout/', views.logout_request, name='logout'),
+
 
     # path for dealer reviews view
     path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_details'),
@@ -27,9 +32,9 @@ urlpatterns = [
     #path for get_cars
     path(route='get_cars', view=views.get_cars, name ='getcars'),
 
+ 
     #path for get_dealerships
-    
-        path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
+    path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
     
     #path for get_dealer_details
     path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),    
