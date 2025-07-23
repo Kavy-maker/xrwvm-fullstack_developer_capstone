1ff80eac51737ab9c9274ec657773f248467e1a5
Kavy-maker <kavitayvonne@gmail.com>
2025-07-23 06:18:54 -0400

    Initial commit

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -5,7 +5,7 @@ from .models import CarMake, CarModel
 
 
 # Register your models here.
-admin.site.register(CarMake),
+admin.site.register(CarMake)
 admin.site.register(CarModel)
 
 # CarModelInline class
