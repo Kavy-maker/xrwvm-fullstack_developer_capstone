493290bb35fcd91a8712aa92697998e7678d77cd
Kavy-maker <kavitayvone@gmail.com>
2025-07-25 05:50:39 -0400

    Initial commit

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -34,7 +34,9 @@ urlpatterns = [
 
  
     #path for get_dealerships
-    path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
+    #path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
+    path('get_dealers/', views.get_dealerships, name='get_dealers'),
+        path('get_dealers/<str:state>', views.get_dealerships, name='get_dealers_by_state'),
 
 
 
